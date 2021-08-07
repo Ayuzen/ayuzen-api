1fc307871145da6b27a98c33f7b8dce67da780a9
Mukut Brahma <mukutb.mac@gmail.com>
2021-08-07 12:03:21 +0530

    upload config

--- extensions/upload/config/settings.js
+++ extensions/upload/config/settings.js
@@ -1,9 +1,9 @@
 module.exports = {
-    provider: "do",
-    providerOptions: {
-      key: process.env.DO_SPACE_ACCESS_KEY,
-      secret: process.env.DO_SPACE_SECRET_KEY,
-      endpoint: process.env.DO_SPACE_ENDPOINT,
-      space: process.env.DO_SPACE_BUCKET,
-    }
+  provider: process.env.NODE_ENV === 'development' ? 'local' : "do",
+  providerOptions: process.env.NODE_ENV === 'development' ? {} : {
+    key: process.env.DO_SPACE_ACCESS_KEY,
+    secret: process.env.DO_SPACE_SECRET_KEY,
+    endpoint: process.env.DO_SPACE_ENDPOINT,
+    space: process.env.DO_SPACE_BUCKET,
   }
+}
