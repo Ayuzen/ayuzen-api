4851df510d3def05590f61bff3d7b06bba472e3d
Mukut Brahma <mukutb.mac@gmail.com>
2022-03-05 01:13:17 +0530

    server config changed

--- config/server.js
+++ config/server.js
@@ -1,6 +1,8 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
+  // url: env('', 'https://a162-117-205-212-55.ngrok.io'),
+  url: env('', 'https://admin.ayuzen.org'),
   admin: {
     auth: {
       secret: env('ADMIN_JWT_SECRET', '73f419a19f4f5e17a948496a980eaa07'),
